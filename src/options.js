dda68c1b15fb8da67294b0882125d2e7e29db91f
Arth Patel <arthpatelf50@gmail.com>
2020-10-19 19:45:32 +0530

    backend method options file

---
+++ src/options.js
@@ -0,0 +1,31 @@
+export const postMethodOptions = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify()
+}
+
+export const deleteMethodOptions = {
+    method: 'DELETE',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify()
+}
+
+export const putMethodOptions = {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify()
+}
+
+export const getMethodOptions = {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify()
+}
